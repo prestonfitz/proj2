e2962886c46be8dfa930004a5119c8a1f3839185
Preston Fitzgerald <prestonfitz@gmail.com>
2024-12-11 20:09:47 -0700

    More updates

--- terraform/modules/rds/main.tf
+++ terraform/modules/rds/main.tf
@@ -10,6 +10,7 @@ resource "aws_db_instance" "donutdb" {
   instance_class       = "db.m5.large"
   username             = "admin" 
   password             = "password" 
+  publicly_accessible  = "true"
   db_subnet_group_name = "donutdb_subnet_group" 
   vpc_security_group_ids = [var.db_security_group_id] 
   skip_final_snapshot = true
